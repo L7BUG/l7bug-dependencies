8e4dac713b98e183effc4980a86cb930207a0e7d
L7-BUG <ll789y@gmail.com>
2025-11-07 11:19:00 +0800

    1.0.5-SNAPSHOT

---
+++ l7bug-database-spring-boot-starter/src/main/java/com/l7bug/database/config/DatabaseType.java
@@ -0,0 +1,19 @@
+package com.l7bug.database.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * DatabaseType
+ *
+ * @author Administrator
+ */
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "l7bug.database")
+public class DatabaseType {
+	private DbType dbType = DbType.POSTGRE_SQL;
+	private Long rootId = -1L;
+}
---
+++ l7bug-database-spring-boot-starter/src/main/java/com/l7bug/database/dataobject/BaseDo.java
@@ -0,0 +1,43 @@
+package com.l7bug.database.dataobject;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * BaseDo
+ *
+ * @author Administrator
+ * @since 2025/8/20 14:06
+ */
+@Data
+public class BaseDo implements Serializable {
+	@Serial
+	private static final long serialVersionUID = -7865608655588766372L;
+
+	@TableId
+	private Long id;
+
+	@TableField(fill = FieldFill.INSERT)
+	private Long createBy;
+
+	@TableField(fill = FieldFill.INSERT_UPDATE)
+	private Long updateBy;
+
+	@TableField(fill = FieldFill.INSERT)
+	private LocalDateTime createTime;
+
+	@TableField(fill = FieldFill.INSERT_UPDATE)
+	private LocalDateTime updateTime;
+
+	/**
+	 * 删除标识 0：未删除 1：已删除
+	 */
+	@TableField(fill = FieldFill.INSERT)
+	private Boolean delFlag;
+}
