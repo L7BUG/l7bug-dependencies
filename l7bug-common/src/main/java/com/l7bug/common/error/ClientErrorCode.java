7398c1de5f8d13647d58a8f3663a63f5743569c9
L7-BUG <ll789y@gmail.com>
2025-11-10 14:58:00 +0800

    1.0.8-SNAPSHOT

--- l7bug-common/src/main/java/com/l7bug/common/error/ClientErrorCode.java
+++ l7bug-common/src/main/java/com/l7bug/common/error/ClientErrorCode.java
@@ -12,7 +12,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ClientErrorCode implements BaseErrorCode {
-	CLIENT_ERROR("C000000", "客户端错误");
+	CLIENT_ERROR("C000000", "客户端错误"),
+	LOGIN_ERROR("C000001", "登录失败!账号或密码错误!"),
+	USER_IS_DISABLE("C000002", "登录失败!用户已被禁用!请联系管理员解除封禁!"),
+	;
 	private final String code;
 	private final String message;
 }
