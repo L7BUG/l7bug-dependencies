c59da01f3892a4f1d74a040890ab257e5d01fef7
L7-BUG <ll789y@gmail.com>
2025-11-13 09:44:56 +0800

    更新状态码

--- l7bug-common/src/main/java/com/l7bug/common/error/ClientErrorCode.java
+++ l7bug-common/src/main/java/com/l7bug/common/error/ClientErrorCode.java
@@ -13,11 +13,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ClientErrorCode implements BaseErrorCode {
 	CLIENT_ERROR("C000000", "客户端错误"),
+	/*认证相关问题 0开头*/
 	NOT_AUTHENTICATION("C000001", "未登录!请登录后访问!"),
 	ACCESS_DENIED("C000003", "未授权禁止访问!请联系管理员为你授权!"),
 	LOGIN_ERROR("C000004", "登录失败!账号或密码错误!"),
 	USER_IS_DISABLE("C000005", "登录失败!用户已被禁用!请联系管理员解除封禁!"),
 	USER_NOT_NULL("C000006", "注册失败,该用户已被注册"),
+	/*客户端非法操作 1开头*/
+	DATA_IS_NULL("C100000", "操作的数据不存在!"),
 	;
 	private final String code;
 	private final String message;
