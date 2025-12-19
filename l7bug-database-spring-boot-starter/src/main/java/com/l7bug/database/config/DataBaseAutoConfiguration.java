d0acf7f39523318a7e064424bad10ffab29df9b9
L7-BUG <ll789y@gmail.com>
2025-12-19 14:44:39 +0800

    refactor(config): 移除审计器提供者中的冗余参数
    - 删除了 `auditorProvider` 方法中未使用的 `CurrentUserId` 参数
    - 简化了 `AuditorAware` Bean 的定义逻辑
    - 保持了获取当前用户ID的功能不变

--- l7bug-database-spring-boot-starter/src/main/java/com/l7bug/database/config/DataBaseAutoConfiguration.java
+++ l7bug-database-spring-boot-starter/src/main/java/com/l7bug/database/config/DataBaseAutoConfiguration.java
@@ -80,7 +80,7 @@ public class DataBaseAutoConfiguration {
 	}
 
 	@Bean
-	public AuditorAware<Long> auditorProvider(CurrentUserId currentUserId) {
+	public AuditorAware<Long> auditorProvider() {
 		return () -> Optional.of(currentUserId.getCurrentUserId());
 	}
 }
