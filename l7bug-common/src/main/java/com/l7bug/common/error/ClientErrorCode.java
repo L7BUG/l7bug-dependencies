32c16e6add5467381a4b932b92b5a2556dc0cda5
L7-BUG <ll789y@gmail.com>
2026-01-22 21:49:05 +0800

    refactor(error): 重构错误码枚举分类
    - 将客户端非法操作注释修改为system非法操作
    - 新增coupon非法操作分类及其相关错误码
    - 添加优惠卷模块调用错误和全店铺通用禁止设置固定商品错误码

--- l7bug-common/src/main/java/com/l7bug/common/error/ClientErrorCode.java
+++ l7bug-common/src/main/java/com/l7bug/common/error/ClientErrorCode.java
@@ -20,11 +20,15 @@ public enum ClientErrorCode implements BaseErrorCode {
 	USER_IS_DISABLE("C000005", "登录失败!用户已被禁用!请联系管理员解除封禁!"),
 	USER_NOT_NULL("C000006", "注册失败,该用户已被注册"),
 	USER_PASSWORD_IS_ERROR("C000007", "用户密码错误!"),
-	/*客户端非法操作 1开头*/
+	/*system非法操作 1开头*/
 	DATA_IS_NULL("C100000", "操作的数据不存在!"),
 	CHILDREN_IS_NOT_NULL("C100001", "子节点不为空!"),
 	FATHER_IS_NOT_FOLDER("C100002", "父节点的类型不为文件夹!"),
 	FATHER_IS_NOT_PAGE("C100003", "父节点的类型不为页面"),
+
+	/*coupon非法操作 2开头*/
+	COUPON_ERROR("C200000","优惠卷模块调用错误"),
+	ALL_PLATFORM_PRODUCT_DISABLE_OPTION("C200001","全店铺通用禁止设置固定商品"),
 	;
 	private final String code;
 	private final String message;
