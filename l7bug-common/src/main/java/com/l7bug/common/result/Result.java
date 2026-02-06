b82b9f8c0ef1ce37fea0c6559debf61ba46865b5
L7-BUG <ll789y@gmail.com>
2026-02-06 17:36:44 +0800

    feat(common): 添加请求ID字段支持链路追踪
    - 添加了requestId字段用于链路追踪
    - 为code、message、data字段补充了JavaDoc注释
    - requestID可用于查看日志时的请求关联追踪

--- l7bug-common/src/main/java/com/l7bug/common/result/Result.java
+++ l7bug-common/src/main/java/com/l7bug/common/result/Result.java
@@ -15,9 +15,21 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Result<T> {
 	public final static String SUCCESS = "0";
+	/**
+	 * 请求ID,用于链路追踪,查看日志时使用
+	 */
 	private String requestId;
+	/**
+	 * 状态码,0成功
+	 */
 	private String code;
+	/**
+	 * 响应信息
+	 */
 	private String message;
+	/**
+	 * 响应数据
+	 */
 	private T data;
 
 	public boolean isSuccess() {
